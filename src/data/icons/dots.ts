9986acbd13e87095e80076ca75e82735aaa8ba09
Etienne Dldc <etienne@macbook-pro-de-etienne.home>
2018-09-13 03:59:50 +0200

    add dots

---
+++ src/data/icons/dots.ts
@@ -0,0 +1,24 @@
+import TriangularGrid from '../../state/TriangularGrid';
+
+const dots: Array<TriangularGrid.TriangularCoordinate> = [
+  { x: -2, y: 1, side: 'l' },
+  { x: -1, y: 1, side: 'r' },
+  { x: -1, y: 1, side: 'l' },
+  { x: -1, y: 2, side: 'r' },
+  { x: -2, y: 2, side: 'l' },
+  { x: -2, y: 2, side: 'r' },
+  { x: 2, y: 0, side: 'r' },
+  { x: 2, y: 0, side: 'l' },
+  { x: 3, y: 0, side: 'r' },
+  { x: 3, y: -1, side: 'l' },
+  { x: 3, y: -1, side: 'r' },
+  { x: 2, y: -1, side: 'l' },
+  { x: 1, y: -2, side: 'r' },
+  { x: 0, y: -2, side: 'r' },
+  { x: 0, y: -3, side: 'l' },
+  { x: 1, y: -3, side: 'r' },
+  { x: 1, y: -3, side: 'l' },
+  { x: 0, y: -2, side: 'l' },
+];
+
+export default dots;
